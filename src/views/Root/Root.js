fd7e5b3106ed325da4b2a3039585c049aa8a09a1
Shaqal7 <shaqal7@gmail.com>
2020-02-26 16:06:00 +0100

    Globalne style

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const Root = () => (
   <div>
+    <GlobalStyle />
     <h1>Hello</h1>
-    <Button width="100px">close / save</Button>
+    <Button>close / save</Button>
     <Button secondary>remove</Button>
   </div>
 );
