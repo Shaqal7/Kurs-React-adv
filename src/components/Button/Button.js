b1fee23d33e350010da4d905d786f7a7dd511dfe
Shaqal7 <shaqal7@gmail.com>
2020-02-27 10:40:11 +0100

    Storybook knobs

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,20 +2,20 @@ import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   padding: 0;
-  background-color: #ffd82b;
+  background-color: ${({ color }) => color || 'hsl(49, 100%, 58%)'};
   width: ${({ width }) => width || '220px'};
   height: 47px;
   border: none;
   border-radius: 50px;
   font-family: 'Montserrat';
-  font-weight: 500;
+  font-weight: 600;
   font-size: 16px;
   text-transform: uppercase;
 
   ${({ secondary }) =>
     secondary &&
     css`
-      background-color: #e6e6e6;
+      background-color: hsl(0, 0%, 90%);
       width: 105px;
       height: 30px;
       font-size: 10px;
