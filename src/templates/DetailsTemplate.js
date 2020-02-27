bee462632096e29198cd024be6c7d7ebe6284331
Shaqal7 <shaqal7@gmail.com>
2020-02-27 16:33:00 +0100

    Widok szczegółów notatki

---
+++ src/templates/DetailsTemplate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import UserPageTemplate from './UserPageTemplate';
+
+const DetailsTemplate = () => (
+  <UserPageTemplate>
+    <h1>Note</h1>
+    <p>lorem</p>
+    <Link to="/">go back</Link>
+  </UserPageTemplate>
+);
+
+export default DetailsTemplate;
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import MainTemplate from 'templates/MainTemplate';
 import Notes from 'views/Notes';
 import Twitters from 'views/Twitters';
 import Articles from 'views/Articles';
+import DetailsPage from './DetailsPage';
 
 const Root = () => (
   <BrowserRouter>
     <MainTemplate>
       <Switch>
-        <Route exact path="/" component={Notes} />
-        <Route path="/twitters" component={Twitters} />
-        <Route path="/articles" component={Articles} />
+        <Route exact path="/" render={() => <Redirect to="/notes" />} />
+        <Route exact path="/notes" component={Notes} />
+        <Route path="/notes/:id" component={DetailsPage} />
+        <Route exact path="/twitters" component={Twitters} />
+        <Route exact path="/articles" component={Articles} />
       </Switch>
     </MainTemplate>
   </BrowserRouter>
